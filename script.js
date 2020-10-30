fbd61e0494fbacb4c20f7337ff363be2b89c5d6a
Jay Milton <jay@Jays-MacBook-Pro.local>
2020-10-30 13:56:48 -0400

    js functions to check if correct answer is picked

--- script.js
+++ script.js
@@ -5,29 +5,56 @@ fetch("./json/Apprentice_TandemFor400_Data.json")
     const jsonStr = JSON.stringify(data);
     const jsonObj = JSON.parse(jsonStr);
 
-
-
-    console.log(jsonObj);
-
     let questionAndAnswer = jsonObj[Object.keys(jsonObj)[0]];
     let question = questionAndAnswer["question"];
     let incorrectAnswer = questionAndAnswer["incorrect"];
     let correctAnswer = questionAndAnswer["correct"];
-    let choices = [].concat(incorrectAnswer, correctAnswer)
+    let choicesArr = [].concat(incorrectAnswer, correctAnswer)
     let answerDiv = document.querySelector('.answer-btns');
 
 
-    document.querySelector('.question').innerHTML = question;
-    
-    choices.forEach(choice => {
-        let choiceInput = `
-            <label>
-                <input type="radio">
-                ${choice}
-            </label>
-        ` 
-        answerDiv.innerHTML += choiceInput
+   
+
+
+    function renderQuestionAndAnswer(){
+
+        //render question
+        document.querySelector('.question').innerHTML = question;
+
+        //render choices
+        choicesArr.forEach(choice => {
+            let choiceInput = `
+                <button class="choice-btn">
+                    ${choice}
+                </button>
+            ` 
+
+            // <label>
+            //         <input type="radio">
+            //         ${choice}
+            //     </label>
+                answerDiv.innerHTML += choiceInput;
+
+        });
+    }
+
+    renderQuestionAndAnswer();
+
+    function checkAnswer(choice){
+        if(choice === correctAnswer){
+            console.log('Correct')
+        } else (
+            console.log('Try again')
+        )
+    }
+
+    let choicesBtn = document.querySelectorAll('.choice-btn');
+    choicesBtn.forEach(button => {
+        // button.addEventListener('click', checkAnswers);
+        button.addEventListener('click', function(){
+            checkAnswer(button.innerText);
+        });
     });
 
-    console.log(choices);
+
 })
